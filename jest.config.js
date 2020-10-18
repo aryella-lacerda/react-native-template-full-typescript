89a19ffd1ce5fc8ace7b01fc784a871678f2568e
Aryella Lacerda <aryella.lacerda@gmail.com>
2020-10-18 15:04:35 -0300

    chore: add jest coverage path and threshold

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,14 @@
 module.exports = {
   collectCoverage: true,
+  collectCoverageFrom: ['<rootDir>/App'],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
   preset: 'react-native',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   setupFilesAfterEnv: ['<rootDir>/Tests/SetupMocks.tsx', '@testing-library/jest-native/extend-expect'],
